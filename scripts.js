3932754dc07a5784515811292d0002ee709c8f41
GREGORIOtsr <gralfonsotr@gmail.com>
2023-10-03 15:44:39 +0200

    add/script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 console.log(document.title);
+
+// 1.
+document.getElementById("gen-1").innerHTML = "Generasión 1 Pokimon";
+
+// 2.
+document.querySelector(".infocard-list").style.backgroundColor = "black";
+
+// 3.
+console.log(location.href);
+
+// 4.
+console.log(location.hostname);
+
+// 5.
+console.log(document.querySelectorAll('img'));
+
+// 6.
+let cage = document.querySelectorAll('.img-sprite');
+for (i = 0; i < cage.length; i++) {
+    cage[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}
